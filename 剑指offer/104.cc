329cdf02b66a5b7697e19d770af4dd7dcfcdd6a5
chenyuxuan <lccyx002@163.com>
2021-10-30 10:32:20 +0800

    add 1 problems

--- 剑指offer/104.cc
+++ 剑指offer/104.cc
@@ -1,9 +1,23 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int combinationSum4(vector<int> &nums, int target)
 {
-    vector<int> dp(0, target);
+    // dp[i] 以i为目标的数字组合总数
+    // dp[i] += dp[i-j] + 1 
 
-    // dp[n] = n + dp[n - i]
+    vector<int> dp(target + 1);
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++)
+    {
+        for (int &num : nums)
+        {
+            if (num <= i && dp[i - num] < INT64_MAX - dp[i])
+            {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+    return dp[target];
 }
